207415f0deafdf476a85fe50844724bcde4012f9
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-07-25 16:19:59 +0000

    Keep retrying in loop for accurate timing.

--- server/coreweave/service-pvc/testPrediction.sh
+++ server/coreweave/service-pvc/testPrediction.sh
@@ -1,3 +1,15 @@
 url=`./getServiceURL.sh`
 
-curl -d '{"instances": ["That was easy"]}' $url
+keepGoing=1
+
+while [ $keepGoing -gt 0 ]
+do
+	result=`curl -s -d '{"instances": ["That was easy"]}' $url`
+	
+	if [[ $result == *"predictions"* ]]; then
+		echo $result
+		keepGoing=0
+	else
+		echo "Timed out, trying again."
+	fi
+done
