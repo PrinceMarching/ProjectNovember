55a6f391bf9f7085a22f6b24f97cc253856f7898
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-08-06 12:31:22 -0700

    Support for debug param in URL-fetching code.

--- webClient/url.js
+++ webClient/url.js
@@ -1,18 +1,25 @@
 
 
+var urlDebug = false;
+
 
 function getURLAndCall( inURL, inResponseCall ) {
 	var http = new XMLHttpRequest();
 	
-	//console.log( "Starting to get URL: ".concat( inURL ) );
+	if( urlDebug )
+		console.log( "Starting to get URL: ".concat( inURL ) );
 
 	http.open( "GET", inURL );
 	http.send();
 
 	http.onreadystatechange = function () {
 		if( http.readyState === XMLHttpRequest.DONE ) {
-			//console.log( 
-			//	inURL.concat( " got response: " ).concat( http.responseText ) );
+			
+			if( urlDebug )
+				console.log( 
+					inURL.concat( " got response: " ).
+						concat( http.responseText ) );
+			
 			inResponseCall( http.responseText );
 		}
 	};
