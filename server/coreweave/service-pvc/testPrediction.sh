6eeb7cb2359de9b29906cf2de3e8ec6efb22a7e0
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-08-13 15:36:31 +0000

    Re-get service url on timeout (so that test script can be launched safely immediately after brining service back online.)

--- server/coreweave/service-pvc/testPrediction.sh
+++ server/coreweave/service-pvc/testPrediction.sh
@@ -1,9 +1,10 @@
-url=`./getServiceURL.sh`
 
 keepGoing=1
 
 while [ $keepGoing -gt 0 ]
 do
+	url=`./getServiceURL.sh`
+
 	result=`curl -s -d '{"instances": ["That was easy"]}' $url`
 	
 	if [[ $result == *"predictions"* ]]; then
--- server/coreweave/service-pvc/testPrediction2.sh
+++ server/coreweave/service-pvc/testPrediction2.sh
@@ -1,6 +1,5 @@
-url=`./getServiceURL.sh`
 
-prompt="What follows is a conversation between a human and the world's most sophisticated artificial intelligence.  As you can see, the computer displays a shocking amount of cleverness and wit, as the discussion quickly becomes deep and philosophical.\n\nComputer: We can have a deep and meaningful conversation about what really matters in life, if you're willing to cooperate.\n\nHuman: Hello?\n\nComputer: "
+prompt="What follows is a conversation between a human and the world's most sophisticated artificial intelligence.  As you can see, the computer displays a shocking amount of cleverness and wit, as the discussion quickly becomes deep and philosophical.\n\nComputer: We can have a deep and meaningful conversation about what really matters in life, if you're willing to cooperate.\n\nHuman: Hello?\n\nComputer:"
 
 
 keepGoing=1
@@ -17,6 +16,8 @@ do
 	curlData="$curlDataA$curlDataB$curlDataC"
 
 	echo -n "$curlData" > tempFile.txt 
+
+	url=`./getServiceURL.sh`
 	
 	result=`curl -s --trace curlLog.txt -H "Content-Type: application/json" --data-binary '@tempFile.txt' $url`
 	
