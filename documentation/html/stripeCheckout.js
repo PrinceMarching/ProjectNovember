45d3df3ff0c21ea459433b6d2a7bcde68188580b
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-08-20 11:39:50 -0700

    stripe checkout handling more parameters.

--- documentation/html/stripeCheckout.js
+++ documentation/html/stripeCheckout.js
@@ -7,20 +7,39 @@ var publishableKey = "pk_live_51HFoSwLc2fQJ3IRBCJ0eqIwT2W6QOOhlOAhAe2Z8Fob5F1fCr
 
 var stripe = Stripe( publishableKey );
 
-function stripeCheckout( email, numCredits ) {
-	// fixme:
-	// handle email being blank (for buy.php)
-	// handle numCredits by mapping to a stripe price code
+function stripeCheckout( email, num_credits, priceID ) {
+	// default arguments
+	if( num_credits === undefined ) {
+		num_credits = 0;
+	}
+	if( email === undefined ) {
+		email = "";
+	}
+
+	// assume they are buying credits
+	// bounce them back to credits page for same values
+	backURL = 'https://projectdecember.net/credits.php?email='.
+		concat( email ).concat( "&num_credits=" ).concat( num_credits );
+
+	// default arguments
+	// if price_id NOT present, we came from buy.php
+	// so go back there as backURL
+	if( priceID === undefined ) {
+		// base account purchase price ID
+		priceID = "price_1HFpXiLc2fQJ3IRBSuwRoTGA";
+		backURL = 'https://projectdecember.net/buy.php';
+	}
 
 	stripe
 		.redirectToCheckout({
 			lineItems: [
 				// Replace with the ID of your price
-				{price: 'price_1HFpXiLc2fQJ3IRBSuwRoTGA', quantity: 1},
+				{price: priceID, quantity: 1},
 			],
 			mode: 'payment',
 			successUrl: 'https://projectdecember.net/success.php',
-			cancelUrl: 'https://projectdecember.net/buy.php',
+			cancelUrl: backURL,
+			customerEmail: email,
 		})
 		.then(function(result) {
 			// If `redirectToCheckout` fails due to a browser or network
